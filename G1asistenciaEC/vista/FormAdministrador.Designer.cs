5c17d7e585be3b073c1a8eed8124793a8801fc61
Jeremy  Sánchez <156977814+JeremySG31@users.noreply.github.com>
2025-06-27 18:42:57 -0500

    ....

--- G1asistenciaEC/vista/FormAdministrador.Designer.cs
+++ G1asistenciaEC/vista/FormAdministrador.Designer.cs
@@ -56,8 +56,8 @@
             this.cONFIGURACIONACADEMICAToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
-            this.menuStrip1.Size = new System.Drawing.Size(896, 24);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(1195, 28);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -69,7 +69,7 @@
             this.eSTUDIANTEAPODERADOSToolStripMenuItem,
             this.toolStripSeparator1});
             this.mANTENIMIENTOToolStripMenuItem.Name = "mANTENIMIENTOToolStripMenuItem";
-            this.mANTENIMIENTOToolStripMenuItem.Size = new System.Drawing.Size(141, 20);
+            this.mANTENIMIENTOToolStripMenuItem.Size = new System.Drawing.Size(180, 24);
             this.mANTENIMIENTOToolStripMenuItem.Text = "GESTION DE USUARIOS";
             this.mANTENIMIENTOToolStripMenuItem.Click += new System.EventHandler(this.mANTENIMIENTOToolStripMenuItem_Click);
             // 
@@ -77,7 +77,7 @@
             // 
             this.uSUARIOSToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("uSUARIOSToolStripMenuItem.Image")));
             this.uSUARIOSToolStripMenuItem.Name = "uSUARIOSToolStripMenuItem";
-            this.uSUARIOSToolStripMenuItem.Size = new System.Drawing.Size(212, 26);
+            this.uSUARIOSToolStripMenuItem.Size = new System.Drawing.Size(263, 26);
             this.uSUARIOSToolStripMenuItem.Text = "ADMINISTRAR USUARIOS";
             this.uSUARIOSToolStripMenuItem.Click += new System.EventHandler(this.uSUARIOSToolStripMenuItem_Click);
             // 
@@ -85,22 +85,22 @@
             // 
             this.rOLESToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("rOLESToolStripMenuItem.Image")));
             this.rOLESToolStripMenuItem.Name = "rOLESToolStripMenuItem";
-            this.rOLESToolStripMenuItem.Size = new System.Drawing.Size(212, 26);
-            this.rOLESToolStripMenuItem.Text = "ROLES DEFINIDOS";
+            this.rOLESToolStripMenuItem.Size = new System.Drawing.Size(263, 26);
+            this.rOLESToolStripMenuItem.Text = "MANIPULAR ROLES";
             this.rOLESToolStripMenuItem.Click += new System.EventHandler(this.rOLESToolStripMenuItem_Click);
             // 
             // eSTUDIANTEAPODERADOSToolStripMenuItem
             // 
             this.eSTUDIANTEAPODERADOSToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("eSTUDIANTEAPODERADOSToolStripMenuItem.Image")));
             this.eSTUDIANTEAPODERADOSToolStripMenuItem.Name = "eSTUDIANTEAPODERADOSToolStripMenuItem";
-            this.eSTUDIANTEAPODERADOSToolStripMenuItem.Size = new System.Drawing.Size(212, 26);
+            this.eSTUDIANTEAPODERADOSToolStripMenuItem.Size = new System.Drawing.Size(263, 26);
             this.eSTUDIANTEAPODERADOSToolStripMenuItem.Text = "ASIGNAR APODERADOS";
             this.eSTUDIANTEAPODERADOSToolStripMenuItem.Click += new System.EventHandler(this.eSTUDIANTEAPODERADOSToolStripMenuItem_Click);
             // 
             // toolStripSeparator1
             // 
             this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(209, 6);
+            this.toolStripSeparator1.Size = new System.Drawing.Size(260, 6);
             // 
             // gESTIONDEMATRICULASToolStripMenuItem
             // 
@@ -108,14 +108,14 @@
             this.aDMINISTRARMATRICULASToolStripMenuItem,
             this.cURSOSPORToolStripMenuItem});
             this.gESTIONDEMATRICULASToolStripMenuItem.Name = "gESTIONDEMATRICULASToolStripMenuItem";
-            this.gESTIONDEMATRICULASToolStripMenuItem.Size = new System.Drawing.Size(156, 20);
+            this.gESTIONDEMATRICULASToolStripMenuItem.Size = new System.Drawing.Size(197, 24);
             this.gESTIONDEMATRICULASToolStripMenuItem.Text = "GESTION DE MATRICULAS";
             // 
             // aDMINISTRARMATRICULASToolStripMenuItem
             // 
             this.aDMINISTRARMATRICULASToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("aDMINISTRARMATRICULASToolStripMenuItem.Image")));
             this.aDMINISTRARMATRICULASToolStripMenuItem.Name = "aDMINISTRARMATRICULASToolStripMenuItem";
-            this.aDMINISTRARMATRICULASToolStripMenuItem.Size = new System.Drawing.Size(227, 26);
+            this.aDMINISTRARMATRICULASToolStripMenuItem.Size = new System.Drawing.Size(280, 26);
             this.aDMINISTRARMATRICULASToolStripMenuItem.Text = "ADMINISTRAR MATRICULAS";
             this.aDMINISTRARMATRICULASToolStripMenuItem.Click += new System.EventHandler(this.aDMINISTRARMATRICULASToolStripMenuItem_Click);
             // 
@@ -123,7 +123,7 @@
             // 
             this.cURSOSPORToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("cURSOSPORToolStripMenuItem.Image")));
             this.cURSOSPORToolStripMenuItem.Name = "cURSOSPORToolStripMenuItem";
-            this.cURSOSPORToolStripMenuItem.Size = new System.Drawing.Size(227, 26);
+            this.cURSOSPORToolStripMenuItem.Size = new System.Drawing.Size(280, 26);
             this.cURSOSPORToolStripMenuItem.Text = "CURSOS-MATRICULADOS";
             this.cURSOSPORToolStripMenuItem.Click += new System.EventHandler(this.cURSOSPORToolStripMenuItem_Click);
             // 
@@ -135,14 +135,14 @@
             this.gRADOSYSECCIONESToolStripMenuItem,
             this.tURNOSToolStripMenuItem});
             this.cONFIGURACIONACADEMICAToolStripMenuItem.Name = "cONFIGURACIONACADEMICAToolStripMenuItem";
-            this.cONFIGURACIONACADEMICAToolStripMenuItem.Size = new System.Drawing.Size(185, 20);
+            this.cONFIGURACIONACADEMICAToolStripMenuItem.Size = new System.Drawing.Size(227, 24);
             this.cONFIGURACIONACADEMICAToolStripMenuItem.Text = "CONFIGURACION ACADEMICA";
             // 
             // aÑOSLECTIVOSToolStripMenuItem1
             // 
             this.aÑOSLECTIVOSToolStripMenuItem1.Image = ((System.Drawing.Image)(resources.GetObject("aÑOSLECTIVOSToolStripMenuItem1.Image")));
             this.aÑOSLECTIVOSToolStripMenuItem1.Name = "aÑOSLECTIVOSToolStripMenuItem1";
-            this.aÑOSLECTIVOSToolStripMenuItem1.Size = new System.Drawing.Size(198, 26);
+            this.aÑOSLECTIVOSToolStripMenuItem1.Size = new System.Drawing.Size(243, 26);
             this.aÑOSLECTIVOSToolStripMenuItem1.Text = "AÑOS LECTIVOS";
             this.aÑOSLECTIVOSToolStripMenuItem1.Click += new System.EventHandler(this.aÑOSLECTIVOSToolStripMenuItem1_Click);
             // 
@@ -150,7 +150,7 @@
             // 
             this.cURSOSToolStripMenuItem1.Image = ((System.Drawing.Image)(resources.GetObject("cURSOSToolStripMenuItem1.Image")));
             this.cURSOSToolStripMenuItem1.Name = "cURSOSToolStripMenuItem1";
-            this.cURSOSToolStripMenuItem1.Size = new System.Drawing.Size(198, 26);
+            this.cURSOSToolStripMenuItem1.Size = new System.Drawing.Size(243, 26);
             this.cURSOSToolStripMenuItem1.Text = "CURSOS";
             this.cURSOSToolStripMenuItem1.Click += new System.EventHandler(this.cURSOSToolStripMenuItem1_Click);
             // 
@@ -158,7 +158,7 @@
             // 
             this.gRADOSYSECCIONESToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("gRADOSYSECCIONESToolStripMenuItem.Image")));
             this.gRADOSYSECCIONESToolStripMenuItem.Name = "gRADOSYSECCIONESToolStripMenuItem";
-            this.gRADOSYSECCIONESToolStripMenuItem.Size = new System.Drawing.Size(198, 26);
+            this.gRADOSYSECCIONESToolStripMenuItem.Size = new System.Drawing.Size(243, 26);
             this.gRADOSYSECCIONESToolStripMenuItem.Text = "GRADOS Y SECCIONES";
             this.gRADOSYSECCIONESToolStripMenuItem.Click += new System.EventHandler(this.gRADOSYSECCIONESToolStripMenuItem_Click);
             // 
@@ -166,24 +166,24 @@
             // 
             this.tURNOSToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("tURNOSToolStripMenuItem.Image")));
             this.tURNOSToolStripMenuItem.Name = "tURNOSToolStripMenuItem";
-            this.tURNOSToolStripMenuItem.Size = new System.Drawing.Size(198, 26);
+            this.tURNOSToolStripMenuItem.Size = new System.Drawing.Size(243, 26);
             this.tURNOSToolStripMenuItem.Text = "TURNOS";
             this.tURNOSToolStripMenuItem.Click += new System.EventHandler(this.tURNOSToolStripMenuItem_Click);
             // 
             // panelContenido
             // 
-            this.panelContenido.Location = new System.Drawing.Point(21, 25);
-            this.panelContenido.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panelContenido.Location = new System.Drawing.Point(28, 31);
+            this.panelContenido.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panelContenido.Name = "panelContenido";
-            this.panelContenido.Size = new System.Drawing.Size(866, 418);
+            this.panelContenido.Size = new System.Drawing.Size(1155, 514);
             this.panelContenido.TabIndex = 1;
             // 
             // btnCerrarSesion
             // 
-            this.btnCerrarSesion.Location = new System.Drawing.Point(762, 2);
-            this.btnCerrarSesion.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnCerrarSesion.Location = new System.Drawing.Point(1016, 2);
+            this.btnCerrarSesion.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnCerrarSesion.Name = "btnCerrarSesion";
-            this.btnCerrarSesion.Size = new System.Drawing.Size(98, 23);
+            this.btnCerrarSesion.Size = new System.Drawing.Size(131, 28);
             this.btnCerrarSesion.TabIndex = 2;
             this.btnCerrarSesion.Text = "Cerrar Sesión";
             this.btnCerrarSesion.UseVisualStyleBackColor = true;
@@ -191,14 +191,14 @@
             // 
             // FormAdministrador
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(896, 442);
+            this.ClientSize = new System.Drawing.Size(1195, 544);
             this.Controls.Add(this.btnCerrarSesion);
             this.Controls.Add(this.panelContenido);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "FormAdministrador";
             this.Text = "FormAdministrador";
             this.Load += new System.EventHandler(this.FormAdministrador_Load);
